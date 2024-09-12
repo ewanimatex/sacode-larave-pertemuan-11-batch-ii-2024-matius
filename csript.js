6c00a201344984df0007ddf14bc0381090ab9f0e
matius ewani <ewanimatex@gmail.com>
2024-09-12 04:08:50 -0700

    sacode-laravel-pertemuan-11-batch-ii-2024-matius

---
+++ csript.js
@@ -0,0 +1,4 @@
+const tambahBtn = document.getElementById(tambahBtn);
+const resetBtn = document.getElementById(resizeTo);
+const tambahBtn = document.getElementById(tambahBtn);
+const tambahBtn = document.getElementById(tambahBtn);
---
+++ script.js
@@ -0,0 +1,22 @@
+
+        var x = 10;
+        var y = 10;
+        var z = x*y;
+        function perkalian(x,y){
+              return x*y;
+        }
+
+        perkalian(4,4);
+        perkalian(6,3)
+        perkalian(9,2)
+
+    
+
+
+const btn = document.getElementById("btn");
+const tampilan = document.getElementById(tampilan);
+
+
+btn.onclick = function(tampilan){
+    tampilan.innerHTML = "Welcome Matius";
+}
